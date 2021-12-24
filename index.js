ea442abf9c36e57ddfd31aa4c7d3c62dd73e8e6b
MashfiqulHoque1 <1921648@iub.edu.bd>
2021-12-24 11:59:04 +0600

    Third commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var temp = document.querySelector('.temp');
 var desc = document.querySelector('.desc');
 var clouds = document.querySelector('.clouds');
 var button= document.querySelector('.submit');
+var dynamic = document.querySelector('.image');
 
 
 button.addEventListener('click', function(name){
@@ -13,12 +14,24 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=c
     console.log(data);
   var tempValue = data['main']['temp']- 273.15;
   var nameValue = data['name'];
-  var descValue = data['weather'][0]['description'];
+  var descValue = data['weather'][0]['main'];
 
   main.innerHTML = nameValue;
   desc.innerHTML = "Desc - "+descValue;
   temp.innerHTML = "Temp - "+tempValue.toFixed(2) + "°C";
   input.value ="";
+
+  if(descValue == "Clouds"){
+      dynamic.innerHTML = `<img src="Pics-final/cloudy.jpg" alt="" height="300px" width="300px">`
+  }
+
+  else if(descValue == "Mist"){
+    dynamic.innerHTML = `<img src="Pics-final/mist.jpg" alt="" height="300px" width="300px">`
+}
+
+else if(descValue == "Sunny"){
+    dynamic.innerHTML = `<img src="Pics-final/sunny.jpg" alt="" height="300px" width="300px">`
+}
 
 })
 
